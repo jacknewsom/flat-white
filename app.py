f4ae201db69ed7ba10ad08d2ef4f12b001e6a967
jack <jackmnewsom@gmail.com>
2020-01-10 17:52:13 -0800

    figured out how to not burn coffee

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import asyncio
 import RPi.GPIO as GPIO
 
 app = Flask(__name__)
@@ -8,11 +9,17 @@ coffee_pot = 29
 def index():
     if request.method == 'POST':
         GPIO.output(coffee_pot, GPIO.HIGH)
-        return 'Brewing coffee...'
+        asyncio.run(reset_state(5))
+        return 'Brewing coffee...'    
     return render_template('index.html')
+
+async def reset_state(lifetime):
+    asyncio.sleep(lifetime)
+    GPIO.output(coffee_pot, GPIO.LOW)
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(coffee_pot, GPIO.OUT)
     GPIO.output(coffee_pot, GPIO.LOW)
+    coffee_state = False
     app.run(host='0.0.0.0')
