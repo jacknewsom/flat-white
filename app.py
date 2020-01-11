41f29d53d961a8a3640030d7fb6e4cee0b3932ac
Jack Newsom <jackmnewsom@gmail.com>
2020-01-10 17:39:16 -0800

    Added GPIO things

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+coffee_pot = 29
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return 'Brewing coffee...'    
+        GPIO.output(coffee_pot, GPIO.HIGH)
+        return 'Brewing coffee...'
     return render_template('index.html')
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(coffee_pot, GPIO.OUT)
+    GPIO.output(coffee_pot, GPIO.LOW)
     app.run(host='0.0.0.0')
